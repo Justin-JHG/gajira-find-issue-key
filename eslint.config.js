280006ef65851038b0aa223717e5fec688eb5bf2
Justin Liu <Justin.liu@jhg.com.au>
2026-03-18 20:12:48 +1100

    feat: convert to ESM and upgrade to @actions/core 3.0.0 and ESLint 9
    BREAKING CHANGES:
    - Converted entire codebase from CommonJS to ES modules
    - Added 'type': 'module' to package.json
    - Changed all require() to import statements
    - Changed all module.exports to export statements
    - Updated file imports to include .js extensions
    
    Dependencies upgraded:
    - @actions/core: 1.11.1  3.0.0 (ESM-only, Node 24 support)
    - eslint: 8.57.0  9.39.4 (flat config format)
    - Created eslint.config.js for ESLint 9 flat config
    
    Security:
    - npm audit: 0 vulnerabilities
    - All Dependabot alerts resolved
    
    Package manager:
    - Switched from yarn to npm due to registry issues
    - Added package-lock.json
    - Removed yarn.lock

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+      }
+    },
+    rules: {
+      // Add your custom rules here if needed
+    }
+  }
+];
